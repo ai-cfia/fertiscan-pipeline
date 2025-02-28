4491b1c0d3046d8a2e71ac88d92ed30a5839091d
Bryan Ndjeutcha <ndjeutcha@gmail.com>
2025-02-28 09:39:06 -0500

    fix: analyze function by making it take a settings

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,10 +1,11 @@
 from pipeline.modules.main_module import MainModule
+from pipeline.schemas.settings import Settings
 from pipeline.schemas.inspection import FertilizerInspection  # noqa: F401
 from PIL import Image
 
 def save_text_to_file(text: str, output_path: str):  # pragma: no cover
     """
-    Save text to a file. 
+    Save text to a file.
     """
     with open(output_path, 'w') as output_file:
         output_file.write(text)
@@ -12,19 +13,18 @@ def save_text_to_file(text: str, output_path: str):  # pragma: no cover
 
 def save_image_to_file(image_bytes: bytes, output_path: str):  # pragma: no cover
     """
-    Save the raw byte data of an image to a file. 
+    Save the raw byte data of an image to a file.
     """
     with open(output_path, 'wb') as output_file:
         output_file.write(image_bytes)
 
 
-def analyze(images: list[Image.Image], llm_api_key, llm_api_endpoint, llm_api_deployment_id, ocr_api_key, ocr_api_endpoint) -> FertilizerInspection:
+def analyze(images: list[Image.Image], settings: Settings) -> FertilizerInspection:
     """
     Analyze a fertiliser label using our pipeline module(s).
     It returns the data extracted from the label in a FertiliserForm.
     """
-    predictor = MainModule(
-        llm_api_key, llm_api_endpoint, llm_api_deployment_id, ocr_api_key, ocr_api_endpoint)
+    predictor = MainModule(settings)
 
     # Analyse all the images loaded
     prediction = predictor.forward(images)
